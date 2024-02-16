800c5d4b5ac4ae12d895212f3d4f4cd58d97125d
micdurodola <micdurodola@gmail.com>
2024-02-16 10:57:09 +0000

    setup frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <header>E-commerce</header>
+        <main></main>
+        <footer>All right reserved</footer>
+     </div>
+    </>
+  )
+}
+
+export default App
