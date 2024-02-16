0e26c85809b21c91c9e5736290062bb7efa54fd6
micdurodola <micdurodola@gmail.com>
2024-02-16 11:48:11 +0000

    add boostrap package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { sampleProducts } from './data'
 
 function App() {
 
@@ -7,7 +9,16 @@ function App() {
     <>
       <div>
         <header>E-commerce</header>
-        <main></main>
+        <main>
+          <ul>
+            {sampleProducts.map(product =>(
+               <li key={product.slug}><img src={product.image} alt={product.name} className='product-image' />
+              <h3>{product.name}</h3>
+              <p>${product.price}</p>
+              </li>
+            ))}
+          </ul>
+          </main>
         <footer>All right reserved</footer>
      </div>
     </>
---
+++ frontend/src/types/Product.ts
@@ -0,0 +1,13 @@
+export type Product = {
+    name:string
+    slug:string
+    image:string
+    category:string
+    countInStock:number
+    brand:string
+    price:number
+    description:string
+    rating:number
+    numReviews:number
+
+}
